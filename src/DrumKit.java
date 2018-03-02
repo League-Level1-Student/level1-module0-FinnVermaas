231111c04121b873f8b41c82f7bd2a6fc7c98c17
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-01 17:26:02 -0800

    drumkit

--- src/DrumKit.java
+++ src/DrumKit.java
@@ -21,6 +21,7 @@ import javax.swing.JPanel;
 public class DrumKit extends MouseAdapter {
 
     JLabel drumLabelWithImage;
+JLabel cymbalLabelWithImage;
 
     public static void main(String[] args) throws Exception {
    	 new DrumKit().getGoing();
@@ -45,29 +46,42 @@ public class DrumKit extends MouseAdapter {
     String snare = "snare.jpg";
    	 // 9. Edit the next line to use your String variable
  drumLabelWithImage = createLabelImage(snare);
-   	 
+   	  String cymbal="cymbal.jpg";
+    cymbalLabelWithImage = createLabelImage(cymbal);
+    
    	 // 10. Add the image to the panel
     panel.add(drumLabelWithImage);
+    panel.add(cymbalLabelWithImage);
    	 // 11. Set the layout of the panel to "new GridLayout()"
     panel.setLayout(new GridLayout());
    	 // 12. call the pack() method on the frame
    	 frame.pack();
    	 // 13. add a mouse listener to drumLabelWithImage.
-
+drumLabelWithImage.addMouseListener(this);
+cymbalLabelWithImage.addMouseListener(this);
     }
 
     public void mouseClicked(MouseEvent e) {
    	 // 14. When the mouse is clicked, print "mouse clicked"
 
    	 JLabel drumClicked = (JLabel) e.getSource();
+   	 JLabel cymbalClicked = (JLabel) e.getSource();
    	 // 15. Download a drum sound and drop it into your "default package". You can find it on freesound.org. To download it, log in as leagueofamazing/code4life.
+   	
    	 // 16. If they clicked on the drumImage...
-
+if(drumLabelWithImage.equals(e.getSource())) {
    		 // 17. ...use the playSound method to play a drum sound.
+playSound("drum.wav");
+ 
+}
+if(cymbalLabelWithImage.equals(e.getSource())) {
+		 // 17. ...use the playSound method to play a drum sound.
+playSound("cymbal.wav");
 
-   	 // 18. Add more images to make a drumkit. Remember to add a mouse listener to each one.
+}
+// 18. Add more images to make a drumkit. Remember to add a mouse listener to each one.
+  
     }
-
     private JLabel createLabelImage(String fileName)
 			throws MalformedURLException
 	{
