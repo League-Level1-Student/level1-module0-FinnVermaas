e5240e41119a3aae9967bdd76b7f4ee8c4e04fb5
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-19 18:24:22 -0700

    sound effects

--- src/SoundEffects.java
+++ src/SoundEffects.java
@@ -8,35 +8,52 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class SoundEffects  implements ActionListener{
+	 private void playSound(String fileName) {
+	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+	     sound.play();
+	}
+
+
+
+
 
 	public static void main(String[] args) {
 		SoundEffects se = new SoundEffects();
 		
-		se.playSound("sawing-wood-daniel_simon.wav");
+		
+		se.methodOfHoldingThings();
 	}
 	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JButton button = new JButton();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
 	  AudioClip sound;	
-	private void playSound(String fileName) {
-	      sound = JApplet.newAudioClip(getClass().getResource(fileName)); 
-	     frame.add(panel);
-	     frame.setVisible(true);
-	     button.setText("sawing wood");
-	     button.addActionListener(this);
-	     button.setSize(5000, 5000);
-	     frame.pack();
-	    
-	}
+	
 
-
+void methodOfHoldingThings() {
+	 frame.add(panel);
+     frame.setVisible(true);
+     panel.add(button1);
+     panel.add(button2);
+     button1.setText("sawing wood");
+     button1.addActionListener(this);
+     button1.setSize(50, 50);
+     frame.pack();
+     button2.setText("cat screetch");
+     button2.addActionListener(this);
+     button2.setSize(50, 50);
+     frame.pack();
+}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-	JButton buttonPressed = new JButton();
-		if(buttonPressed .equals(button)) {
-	     sound.play();
+
+		if(arg0.getSource().equals(button1)) {
+	     playSound("sawing-wood-daniel_simon.wav");
+	     }
+		else if(arg0.getSource().equals(button2)) {
+	     playSound("98674__hamface__catyell.wav");
 	     }
 	}
 
